55476e876188df01333743dd7985cb9599f92653
Gino Messmer <g.messmer@ftsoftware.eu>
2016-10-06 14:39:12 +0200

    Fonts and FPS Counter

---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Text FPSText;
+
+    void Update()
+    {
+        FPSText.text = (int)((1 / Time.smoothDeltaTime) * Time.timeScale) + " FPS";
+    }
+}
